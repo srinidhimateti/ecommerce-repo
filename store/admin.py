a91d098c2900a0446a12cf72cbcef47125985f43
Srinidhi <srinidhimateti@gmail.com>
2022-05-23 22:00:32 +0530

    added files

---
+++ store/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+
+from .models import Category, Product
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug':('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','slug','price','in_stock','created']
+    list_filters = ['in_stock','is_active']
+    list_editable = ['price','in_stock']
+    prepopulated_fields = {'slug':('title',)}
