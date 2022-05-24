9e39989de3988f277fa08bdeae619784a8931a73
srinidhimateti <58879479+srinidhimateti@users.noreply.github.com>
2022-05-24 21:22:21 +0530

    Update basket.py

--- basket/basket.py
+++ basket/basket.py
@@ -3,10 +3,9 @@ class Basket():
     A base Basket class, providing some default behaviors that
     can be inherited or overrided, as necessary.
     """
-
     def __init__(self, request):
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number':12345}
+            basket = self.session['skey'] = {}
         self.basket = basket
