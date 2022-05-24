b9f83c8eba777e20188abff7ed6d7001d7843da8
Srinidhi <srinidhimateti@gmail.com>
2022-05-24 20:50:25 +0530

    Home styled

--- basket/basket.py
+++ basket/basket.py
@@ -8,5 +8,5 @@ class Basket():
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
-            basket = self.session['skey'] = {'number':12345}
+            basket = self.session['skey'] = {}
         self.basket = basket
