d3477980f60722112b71f38bf60a76a5a37aee90
Srinidhi <srinidhimateti@gmail.com>
2022-06-01 08:46:38 +0530

    New Commit to Main

--- basket/basket.py
+++ basket/basket.py
@@ -10,3 +10,16 @@ class Basket():
         if 'skey' not in request.session:
             basket = self.session['skey'] = {}
         self.basket = basket
+
+    def add(self, product, qty):
+        """
+        Adding and updating the users basket session data
+        """
+        product_id = str(product.id)
+
+        if product_id in self.basket:
+            self.basket[product_id]['qty'] = qty
+        else:
+            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
+
+        self.save()    
