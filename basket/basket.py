5285f8ab0e2e551c2e150ae1b57d1f5419679560
Srinidhi <srinidhimateti@gmail.com>
2022-06-01 09:06:00 +0530

    Final Commit

--- basket/basket.py
+++ basket/basket.py
@@ -3,23 +3,9 @@ class Basket():
     A base Basket class, providing some default behaviors that
     can be inherited or overrided, as necessary.
     """
-
     def __init__(self, request):
         self.session = request.session
         basket = self.session.get('skey')
         if 'skey' not in request.session:
             basket = self.session['skey'] = {}
         self.basket = basket
-
-    def add(self, product, qty):
-        """
-        Adding and updating the users basket session data
-        """
-        product_id = str(product.id)
-
-        if product_id in self.basket:
-            self.basket[product_id]['qty'] = qty
-        else:
-            self.basket[product_id] = {'price': str(product.price), 'qty': qty}
-
-        self.save()    
--- basket/urls.py
+++ basket/urls.py
@@ -5,5 +5,4 @@ from . import views
 app_name = 'basket'
 urlpatterns = [
     path('', views.basket_summary, name='basket_summary'),
-    path('add/', views.basket_add, name='basket_add'),
 ]
